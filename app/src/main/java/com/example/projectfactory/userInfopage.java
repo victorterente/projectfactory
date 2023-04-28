0fa228a6f6fb2be17f4ed266cb577ea974c53343
victorterente <73474868+victorterente@users.noreply.github.com>
2023-04-28 11:42:46 +0100

    request moderador

--- app/src/main/java/com/example/projectfactory/userInfopage.java
+++ app/src/main/java/com/example/projectfactory/userInfopage.java
@@ -3,6 +3,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +25,7 @@ import java.util.Map;
 public class userInfopage extends AppCompatActivity {
 
     private TextView tvEmailValue, tvPasswordValue, tvNameValue;
+    private Button buttonUpdateStatus; // Add this line
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class userInfopage extends AppCompatActivity {
         tvEmailValue = findViewById(R.id.tvEmailValue);
         tvPasswordValue = findViewById(R.id.tvPasswordValue);
         tvNameValue = findViewById(R.id.tvNameValue);
+        buttonUpdateStatus = findViewById(R.id.btnRequestModeratorPermission); // Add this line
 
         // call API to get user information
         RequestQueue queue = Volley.newRequestQueue(this);
@@ -80,5 +84,46 @@ public class userInfopage extends AppCompatActivity {
 
         // add API request to Volley queue
         queue.add(jsonObjectRequest);
+
+        // set click listener for update status button
+        buttonUpdateStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // update user status to false
+                try {
+                    JSONObject jsonBody = new JSONObject();
+                    jsonBody.put("user_status", false);
+                    jsonBody.put("user_id", userId);
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.PUT, url, jsonBody,
+                            new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    // handle API response
+                                    Toast.makeText(userInfopage.this, "User status updated", Toast.LENGTH_SHORT).show();
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    // handle API error
+                                    Toast.makeText(userInfopage.this, "API error", Toast.LENGTH_SHORT).show();
+                                }
+                            }) {
+                        @Override
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            // Set headers
+                            Map<String, String> headers = new HashMap<>();
+                            headers.put("Content-Type", "application/json");
+                            return headers;
+                        }
+                    };
+
+                    // add API request to Volley queue
+                    queue.add(jsonObjectRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
