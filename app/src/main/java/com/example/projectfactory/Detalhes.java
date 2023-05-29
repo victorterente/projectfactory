cab3f69f5f3a5a7a6be2eda411dd9e574ee0cdea
Pedro Santos <pedrojgmsantos@gmail.com>
2023-05-29 16:27:38 +0100

    c

---
+++ app/src/main/java/com/example/projectfactory/Detalhes.java
@@ -0,0 +1,36 @@
+package com.example.projectfactory;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detalhes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes);
+    }
+
+    public void onClickUserInfo(View v) {
+        Intent intent = new Intent(getApplicationContext(), userInfopage.class);
+        startActivity(intent);
+    }
+
+    public void onClickEventList(View v) {
+        Intent intent = new Intent(getApplicationContext(), ListaDeEventos.class);
+        startActivity(intent);
+    }
+
+    public void onClickDadosEventos(View v) {
+        Intent intent = new Intent(getApplicationContext(), DadosEventos.class);
+        startActivity(intent);
+    }
+
+    public void onClickMeusEventos(View v) {
+        Intent intent = new Intent(getApplicationContext(), MeusEventos.class);
+        startActivity(intent);
+    }
+}
